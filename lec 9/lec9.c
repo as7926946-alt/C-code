be0c4672c0b6a27707e710d87480dedc703c8f27
as7926946-alt <as7926946@gmail.com>
2025-09-09 14:03:39 +0530

    if else

---
+++ lec 9/lec9.c
@@ -0,0 +1,103 @@
+// code for printing numbers in pattern
+
+#include<stdio.h>
+/*int main(){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+}*/
+
+/*int main(){
+    for(int i=0;i<5;i++){
+        for(int j=5;j>i;j--){
+            printf("%d",j);
+        }
+        printf("\n");
+    } 
+    }*/
+
+/*int main(){ 
+    int num=1;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<=i;j++){
+            printf("%d",num++);
+        }
+        printf("\n");
+    }
+
+}*/
+
+/*int main(){
+    int num=65 ;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            printf("%c",num++);
+        }
+        printf("\n");
+    }
+}*/
+
+/*int main(){
+    int num=65;
+    for(int i=0;i<5;i++){
+        for(int j=5;j>i;j--){
+            printf("%C",num++);
+        }
+        printf("\n");
+    }
+}*/
+
+/*int main(){                                 // code for full triangle 
+    for(int i=0;i<4;i++){
+        for(int j=3;j>i;j--){
+            printf(" ");
+        }
+        for(int k=0;k<=i;k++){
+            printf("*");
+        }
+        for(int p=0;p<=i;p++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}*/
+
+/*int main(){                                 // code for full triangle 
+    int num=65;
+    for(int i=0;i<4;i++){
+        for(int j=3;j>i;j--){
+            printf(" ");
+        }
+        for(int k=0;k<=i;k++){
+            printf("%c",num++);
+        }
+        for(int p=0;p<=i;p++){
+            printf("%c",num++);
+        }
+        printf("\n");
+    }
+}*/
+
+/*int main(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<=i;j++){
+            printf(" ");
+        }
+        for(int k=3;k>i;k--){
+           printf("*");
+        }
+        for(int r=3;r>=i;r--){
+            printf("*");
+        }
+        printf("\n");
+    }
+}*/
+
+int main(){
+    int num=65;
+    for(int i=0;i<)
+
+}
